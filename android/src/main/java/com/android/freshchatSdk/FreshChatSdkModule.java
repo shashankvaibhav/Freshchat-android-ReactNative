1c6e3703b6d87c40d6e46351bf2dd2e3bc1acc22
pragadeesh0106 <pragadeesh.n@fr8.in>
2018-02-05 13:55:30 +0530

    Update FreshChatSdkModule.java
    Added show conversation with tags and reset user

--- android/src/main/java/com/android/freshchatSdk/FreshChatSdkModule.java
+++ android/src/main/java/com/android/freshchatSdk/FreshChatSdkModule.java
@@ -9,8 +9,12 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.UiThreadUtil;
 import com.freshchat.consumer.sdk.Freshchat;
+import com.freshchat.consumer.sdk.ConversationOptions;
 import com.freshchat.consumer.sdk.FreshchatConfig;
 import com.freshchat.consumer.sdk.FreshchatUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FreshChatSdkModule extends ReactContextBaseJavaModule {
     private static final String TAG = FreshChatSdkModule.class.getSimpleName();
@@ -41,7 +45,6 @@ public class FreshChatSdkModule extends ReactContextBaseJavaModule {
         FreshchatUser user = Freshchat.getInstance(getReactApplicationContext()).getUser();
         user.setFirstName(firstName).setLastName(lastName).setEmail(email).setPhone(countryCode,phone).setExternalId(userId);
         Freshchat.getInstance(getReactApplicationContext()).setUser(user);
-
     }
 
     @ReactMethod
@@ -50,8 +53,20 @@ public class FreshChatSdkModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void showConversations() {
-        Freshchat.showConversations(getReactApplicationContext());
+    public void showConversations(final String filterTag) {
+        if(filterTag == null){   
+            Freshchat.showConversations(getReactApplicationContext());
+        }else{
+            List<String> tags = new ArrayList<>();
+            tags.add(filterTag);
+
+            ConversationOptions convOptions = new ConversationOptions().filterByTags(tags,filterTag);
+            Freshchat.showConversations(getReactApplicationContext(), convOptions);
+        }
     }
 
+    @ReactMethod
+    public void resetUser() {
+        Freshchat.resetUser(getReactApplicationContext());
+    }
 }
